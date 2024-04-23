4cd4f55c321cad9dac6e10febf1182344c9b24c0
Esumei Moses <archer.me24@gmail.com>
2024-04-22 20:28:12 -0700

    Puppet exec command to kill a running process

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,3 +5,9 @@ exec {'install_flask_with_pip3':
   path    => ['usr/bin', '/usr/local/bin'],
   unless  => 'usr/bin/pip3 show flask | grep Version | grep 2.1.0'
 }
+
+exec {'install_werkzeug_with_pip3':
+  command => '/usr/bin/pip3 install werkzeug==2.1.1',
+  path    => ['usr/bin', '/usr/local/bin'],
+  unless  => 'usr/bin/pip3 show werkzeug | grep Version | grep 2.1.1'
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+#Kill a runnning process
+
+exec { 'kill_process':
+  command => 'kill 17818',
+  onlyif  'ps -p 17818',
+}
