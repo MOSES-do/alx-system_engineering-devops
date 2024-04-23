1aea937c0d7ca0d22319dc5fd012e62a6fbef988
Esumei Moses <archer.me24@gmail.com>
2024-04-22 22:01:06 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,11 @@
 # Install flask using puppet and pip3
-
-exec {'install_flask_with_pip3':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['usr/bin', '/usr/local/bin'],
-  unless  => 'usr/bin/pip3 show flask | grep Version | grep 2.1.0'
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  command  => '/usr/bin/python3 -m pip',
 }
 
-exec {'install_werkzeug_with_pip3':
-  command => '/usr/bin/pip3 install werkzeug==2.1.1',
-  path    => ['usr/bin', '/usr/local/bin'],
-  unless  => 'usr/bin/pip3 show werkzeug | grep Version | grep 2.1.1'
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
