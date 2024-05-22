0bda843607f870ce33a1e24c594f677866a822e1
Esumei Moses <archer.me24@gmail.com>
2024-05-22 13:09:09 -0700

    write list of dictionaries into a csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,39 +1,31 @@
 #!/usr/bin/python3
 """REST API TO RETURN USER INFORMATION"""
 import csv
-import json
 import requests
 import sys
-import urllib.request
 
 if __name__ == "__main__":
     emp_todos = (
         f"https://jsonplaceholder.typicode.com/users/"
         f"{sys.argv[1]}/todos")
-    emp_details = (
-        f"https://jsonplaceholder.typicode.com/users/"
-        f"{sys.argv[1]}")
+    emp_details = "https://jsonplaceholder.typicode.com/users/"
 
-    USER_ID = ""
-    TASK_TITLE = []
-    TASK_COMPLETED_STATUS = []
-    USERNAME = ""
     user_dict = []
-    user_row = {}
-    with urllib.request.urlopen(emp_details) as res:
-        emp = json.loads(res.read())
-        USERNAME = emp.get("username")
-        USER_ID = emp.get("id")
+    emp_name = requests.get(emp_details).json()
+    for i in emp_name:
+        if i['id'] == int(sys.argv[1]):
+            EMPLOYEE_NAME = i['username']
+            USER_ID = i['id']
     todos = requests.get(emp_todos).json()
     for item in todos:
         """create an object of each element in the array"""
         user_row = (
-            {"id": USER_ID, "uname": USERNAME, "title": item.get('completed'),
-                "status": item.get('title')})
+            {"id": USER_ID, "uname": EMPLOYEE_NAME,
+                "title": item.get("completed"), "status": item.get("title")})
         """create a list of objects"""
         user_dict.append(user_row)
 
-    with open('sys.argv[1].csv', 'w', newline='') as csvfile:
+    with open('USER_ID.csv', 'w', newline='') as csvfile:
         fieldnames = user_dict[0].keys()
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
                                 quoting=csv.QUOTE_ALL)
