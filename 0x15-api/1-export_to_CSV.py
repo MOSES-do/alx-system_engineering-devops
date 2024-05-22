7796732891fa197973c1145c754665b3d30ed7a4
Esumei Moses <archer.me24@gmail.com>
2024-05-22 13:29:26 -0700

    u

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python3
-"""REST API TO RETURN USER INFORMATION"""
+"""repellendus veritatis molestias dicta incidunt"""
 import csv
 import requests
 import sys
 
-if __name__ == "__main__":
-    emp_todos = (
-        f"https://jsonplaceholder.typicode.com/users/"
-        f"{sys.argv[1]}/todos")
-    emp_details = "https://jsonplaceholder.typicode.com/users/"
 
-    user_dict = []
-    emp_name = requests.get(emp_details).json()
-    for i in emp_name:
-        if i['id'] == int(sys.argv[1]):
-            EMPLOYEE_NAME = i['username']
-            USER_ID = i['id']
-    todos = requests.get(emp_todos).json()
-    for item in todos:
-        """create an object of each element in the array"""
-        user_row = (
-            {"id": USER_ID, "uname": EMPLOYEE_NAME,
-                "title": item.get("completed"), "status": item.get("title")})
-        """create a list of objects"""
-        user_dict.append(user_row)
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/users/'
 
-    with open('USER_ID.csv', 'w', newline='') as csvfile:
-        fieldnames = user_dict[0].keys()
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
-                                quoting=csv.QUOTE_ALL)
-        """write row headers
-        writer.writeheader()"""
-        """write row values"""
-        writer.writerows(user_dict)
+    par = sys.argv[1]
+    purl = f'https://jsonplaceholder.typicode.com/users/{par}/todos'
+    response_purl = requests.get(purl)
+    response_purl = response_purl.json()
+    response = requests.get(url)
+    response = response.json()
+
+    for key in response:
+        if key['id'] == int(par):
+            EMPLOYEE_NAME = key['username']
+
+    csv_row = []
+
+    for key in response_purl:
+        value = key['completed']
+        anoda_value = key['title']
+        csv_row.append(f'"{par}"')
+        csv_row.append(f',"{EMPLOYEE_NAME}"')
+        csv_row.append(f',"{value}"')
+        csv_row.append(f',"{anoda_value}"')
+        csv_row.append("\n")
+
+    file = f'{par}.csv'
+    with open(file, 'w') as output_file:
+        for cs in csv_row:
+            output_file.write(str(cs))
