334c8fcef87e4beb52cdda1fcbe6d44976886277
Esumei Moses <archer.me24@gmail.com>
2024-05-22 10:08:51 -0700

    Fetch user data using urllib

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,8 +7,12 @@ import sys
 
 
 if __name__ == "__main__":
-    emp_todos = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos/"
-    emp_details = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
+    emp_todos = (
+        f"https://jsonplaceholder.typicode.com/users/"
+        f"{sys.argv[1]}/todos/")
+    emp_details = (
+        f"https://jsonplaceholder.typicode.com/users/"
+        f"{sys.argv[1]}")
 
     tasks = []
     tasks_title = []
@@ -24,15 +28,14 @@ if __name__ == "__main__":
         done_task = item.get("completed")
         if done_task is True:
             completed_tasks += 1
-        title = item.get("title")
-        tasks_title.append(title)
-        for key in item.items():
-            if key == "completed":
-                tasks.append(key)
+            tasks_title.append(item.get("title"))
+        for k in item:
+            if k == "completed":
+                tasks.append(k)
 
     len_of_total_tasks = len(tasks)
 
     print(
         f"Employee {emp_name} is done with tasks "
         f"({completed_tasks}/{len_of_total_tasks}):")
-    [print(f"\t{title}") for title in tasks_title]
+    [print(f"\t {title}") for title in tasks_title]
