d0e42fad4fd8319979ad34e90faf050d6a900d3e
Esumei Moses <archer.me24@gmail.com>
2024-06-05 13:22:21 -0700

    reddit api query

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns the total number of subscribers on a given subreddit"""
-    user_agent='MY API_V1'
+    user_agent = 'MY API_V1'
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": user_agent}
     response = requests.get(url, headers=headers, allow_redirects=False)
